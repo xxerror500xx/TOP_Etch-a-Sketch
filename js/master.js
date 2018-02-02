6c9596474ece8b349c268a9ad24a51e45fb676a2
xxerror500xx <cliff.minks@gmail.com>
2018-02-02 15:52:29 -0600

    Add responsive grid creation

--- js/master.js
+++ js/master.js
@@ -1,27 +1,42 @@
 var options = {
   defaults: {
-    length: 4, // Number of rows
-    width: 4,  // Number of colums
+    length: 6, // Number of rows
+    width: 6, // Number of colums
     bgColor: "#ffffff",
     fgColor: "#000000",
     effect: "none"
   },
 
-  displayGridResolution: function (length = this.defaults.length, width = this.defaults.width) {
+  displayGridResolution: function(length = this.defaults.length, width = this.defaults.width) {
     $("#resolution-get").text(length + " X " + width);
   },
-  displayBG: function (bgColor = this.defaults.bgColor) {
+  displayBG: function(bgColor = this.defaults.bgColor) {
     $("#color-bg-get").text(bgColor);
     $("#background").val(bgColor);
   },
-  displayFG: function (fgColor = this.defaults.fgColor) {
+  displayFG: function(fgColor = this.defaults.fgColor) {
     $("#color-fg-get").text(fgColor);
     $("#foreground").val(fgColor);
   },
-  displayEffect: function (effect = this.defaults.effect) {
+  displayEffect: function(effect = this.defaults.effect) {
     $("#color-ef-get").text(effect);
-  }
-  resetToDefault: function () {
+  },
+  clearGrid: function() {
+    $("#grid").empty();
+  },
+  makeGrid: function(rows = this.defaults.length, columns = this.defaults.width) {
+    for (var i = 0; i < rows; i++) {
+      $("#grid").append("<div id='gr-" + i + "' class='row'></div>");
+      var height = $('#gr-' + i).width() / columns;
+      console.log(height);
+      for (var j = 0; j < columns; j++) {
+        $("#gr-" + i).append("<div id='cell-" + j + "' class='col  bg-success h-100'></div>");
+        $("#gr-" + i).css('height', height + 'px')
+      }
+    }
+
+  },
+  resetToDefault: function() {
     options.displayGridResolution();
     options.displayBG();
     options.displayFG();
@@ -30,6 +45,10 @@ var options = {
   }
 };
 
-$("document").ready(function () {
+$("document").ready(function() {
   options.resetToDefault();
+});
+$(window).resize(function() {
+  options.clearGrid();
+  options.makeGrid();
 });
