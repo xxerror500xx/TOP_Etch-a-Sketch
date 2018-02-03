5beb89aca946b51773aeaaccf8b7fb9c350aa2d5
xxerror500xx <cliff.minks@gmail.com>
2018-02-02 20:12:09 -0600

    Add basic hover effcet to grid

--- js/master.js
+++ js/master.js
@@ -1,12 +1,11 @@
 var options = {
   defaults: {
-    length: 6, // Number of rows
-    width: 6, // Number of colums
+    length: 10, // Number of rows
+    width: 10, // Number of colums
     bgColor: "#ffffff",
     fgColor: "#000000",
     effect: "none"
   },
-
   displayGridResolution: function(length = this.defaults.length, width = this.defaults.width) {
     $("#resolution-get").text(length + " X " + width);
   },
@@ -24,14 +23,21 @@ var options = {
   clearGrid: function() {
     $("#grid").empty();
   },
-  makeGrid: function(rows = this.defaults.length, columns = this.defaults.width) {
+  makeGrid: function(rows = this.defaults.length, columns = this.defaults.width, bgColor = this.defaults.bgColor, fgColor = this.defaults.fgColor) {
+    options.clearGrid()
     for (var i = 0; i < rows; i++) {
       $("#grid").append("<div id='gr-" + i + "' class='row'></div>");
       var height = $('#gr-' + i).width() / columns;
-      console.log(height);
+      $("#gr-" + i).css('height', height + 'px')
       for (var j = 0; j < columns; j++) {
-        $("#gr-" + i).append("<div id='cell-" + j + "' class='col  bg-success h-100'></div>");
-        $("#gr-" + i).css('height', height + 'px')
+        $("#gr-" + i).append("<div id='cell-" + j + "' class='col h-100'></div>");
+        var column = $("#gr-" + i + " #cell-" + j);
+        column.css('background-color', bgColor);
+        column.hover(
+          function() {
+            $( this ).css('background-color', fgColor);
+          }
+        );
       }
     }
 
@@ -46,7 +52,7 @@ var options = {
 };
 
 $("document").ready(function() {
-  options.resetToDefault();
+  // options.resetToDefault();
 });
 $(window).resize(function() {
   options.clearGrid();
